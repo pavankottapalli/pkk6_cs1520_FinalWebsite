4087679a68c0f3901873de1ee69f930e63c1cdf9
pvn03 <pvn03@yahoo.com>
2017-04-26 13:22:31 -0400

    pkk6_CS1520_PersonalWebsite
    Completed Website

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE HTML>
+<!--
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Pavan K. Kottapalli's Website</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body id="top">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+					<h1><strong>I am Pavan K. Kottapalli</strong>,</br>
+					a Computer Engineering Student at<br />
+					the <a href="http://pitt.edu">University of Pittsburgh</a>.</h1>
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- One -->
+					<section id="one">
+						<header class="major">
+							<h3>Sometimes I feel intelligent.  <br />
+							  Other times I have to sing the ABCs in my head to remember which letter comes next.</h3>
+						</header>
+						<p>My name is Pavan K. Kottapalli and I am currently a senior computer engineering student at the University of Pittsburgh. Born and raised in the bay area, California I moved to Pittsburgh, Pa for college. I enjoy the outdoors, cooking, and embracing new experiences. I also enjoy computers and working on personal and academic projects. Currently I am working on developin a video game using unity.</p>
+						
+						<ul class="actions">
+							<li><a href="http://pitt.edu/~pkk6/" class="button">Additional Info.</a></li>
+						</ul>
+					</section>
+
+				<!-- Two -->
+					<section id="two">
+						<h2>Recent Work</h2>
+						<div class="row">
+							<article class="6u 12u$(xsmall) work-item">
+								<a href="images/fulls/01.jpg" class="image fit thumb"><img src="images/thumbs/01.jpg" alt="" /></a>
+								<h3>Computer Org. and Assembly Lang. - Project 3</h3>
+								<p>Writing assembly language to a (console) screen.</p>
+							</article>
+							<article class="6u$ 12u$(xsmall) work-item">
+								<a href="images/fulls/02.jpg" class="image fit thumb"><img src="images/thumbs/02.jpg" alt="" /></a>
+								<h3>Adv. Digital Design - Project 4</h3>
+								<p>Building a 16bit CPU using visual circuit design software.</p>
+							</article>
+							<article class="6u 12u$(xsmall) work-item">
+								<a href="images/fulls/03.jpg" class="image fit thumb"><img src="images/thumbs/03.jpg" alt="" /></a>
+								<h3>Pitt Mobile App Challenge</h3>
+								<p>MotiveU. Rewards students for achieving personal goals.</p>
+							</article>
+							<article class="6u$ 12u$(xsmall) work-item">
+								<a href="images/fulls/04.jpg" class="image fit thumb"><img src="images/thumbs/04.jpg" alt="" /></a>
+								<h3>Software Engineering - Semester Project</h3>
+								<p>Creating a train control system using C#. Group Project.</p>
+							</article>
+							<article class="6u 12u$(xsmall) work-item">
+								<a href="images/fulls/05.jpg" class="image fit thumb"><img src="images/thumbs/05.jpg" alt="" /></a>
+								<h3>Senior Design Project - Semester Project</h3>
+								<p>Created a smart mirror using a tablet and a two way mirror.</p>
+							</article>
+							<article class="6u$ 12u$(xsmall) work-item">
+								<a href="images/fulls/06.jpg" class="image fit thumb"><img src="images/thumbs/06.jpg" alt="" /></a>
+								<h3>Programming Lang. for Web Applications</h3>
+								<p>Created a self promotion personal website. </p>
+							</article>
+						</div>
+						<ul class="actions">
+							<li><a href="images/fulls/07.jpg" class="button">My Resume</a></li>
+						</ul>
+					</section>
+
+				<!-- Three -->
+					<section id="three">
+						<h2>Get In Touch</h2>
+						<p>Please dont hesitiate to get in touch with me and view my work through any of my social media platform links. For more comments, questions, and more information, contact me though the option below! Fill up all fiends.</p>
+						<div class="row">
+							<div class="8u 12u$(small)">
+								<form method="post" id="contact" class="contact-form">
+									<div class="row uniform 50%">
+										<div class="6u 12u$(xsmall)"><input type="text" name="cf_name" id="name" placeholder="Name" /></div>
+										<div class="6u$ 12u$(xsmall)"><input type="email" name="cf_email" id="email" placeholder="Email" /></div>
+										<div class="12u$"><textarea name="cf_message" id="message" placeholder="Message" rows="4"></textarea></div>
+									</div>
+								</form>
+								<form action="Test.html">
+   									 <input type="submit" value="Send Message" />
+								</form>
+								<ul class="actions">
+									<!-- <li><input action="Text.html" type="submit" id="contact-submit" value="Send Message_Test" /></li> -->
+								</ul>
+							</div>
+							<div class="4u$ 12u$(small)">
+								<ul class="labeled-icons">
+									<li>
+										<h3 class="icon fa-home"><span class="label">Address</span></h3>
+										734 S. Millvale Ave.<br />
+										Pittsburgh, PA 15213<br />
+										United States
+									</li>
+									<li>
+										<h3 class="icon fa-mobile"><span class="label">Phone</span></h3>
+										412-645-1947
+									</li>
+									<li>
+										<h3 class="icon fa-envelope-o"><span class="label">Email</span></h3>
+										<a href="#">pkk6@pitt.edu</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="https://twitter.com/" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://github.com/pvn03" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="https://www.facebook.com/pvn03" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/?hl=en" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					</ul>
+			<!--		<ul class="copyright">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul> -->
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
